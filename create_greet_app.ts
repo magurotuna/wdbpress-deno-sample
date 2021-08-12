e142473f51c2dfb909916101a8961bf90044e12d
Yusuke Tanaka <yusuktan@maguro.dev>
2021-08-13 01:12:01 +0900

    add comment about how to run oak on 1.13+

--- create_greet_app.ts
+++ create_greet_app.ts
@@ -1,11 +1,18 @@
 import {
   Application,
   helpers,
+  HttpServerStd, // Deno v1.13.0 以降で動かす場合、これが必要
   Router,
 } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 
 export function createApp(): Application {
-  const app = new Application();
+  const app = new Application({
+    serverConstructor: HttpServerStd, // Deno v1.13.0 以降で動かす場合、これが必要
+  });
+  // Deno v1.12.2 以前では
+  // const app = new Application();
+  // で OK
+
   const router = new Router();
 
   router.get("/greet", (ctx) => {
